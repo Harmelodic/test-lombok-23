9bfd54e665e494dd53ed56335541083f03af1468
Harmelodic <matt@harmelodic.com>
2024-09-26 11:19:04 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        LombokClass lombokClass = new LombokClass("Hello World");
+        System.out.println(lombokClass.getMessage());
+    }
+}
